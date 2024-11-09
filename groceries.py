056826df1c660faea3234a01e45f2873a5d4d6fc
Nelson <nelsonespiritu21@gmail.com>
2024-11-09 09:24:58 +0800

    first commit

---
+++ groceries.py
@@ -0,0 +1,12 @@
+print("Hello World")
+name = input("What is your name? ")
+print("Hi " + name)
+first = int(input("What's your first number? "))
+second = int(input("What's your second number? "))
+plus = first + second
+minus = first - second
+product = first * second
+quo = first/second
+print(f"Sum is {sum}, difference is {minus}, product is {product} and the quotient is {quo}")
+age = int(input(f"How old are you {name}? "))
+print(f"Next year you'll be {age+1}")
